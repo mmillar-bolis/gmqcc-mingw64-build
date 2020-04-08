fb5b46b2bede5949c7c31f508ef79a3160235da8
M.Millar <mmillar@bolis.com>
2020-04-07 23:43:31 -0700

    Created respective Windows exe version headers and resource scripts

---
+++ include/qcvm/version.h
@@ -0,0 +1,21 @@
+// version.h
+
+// Global definitions
+//#define VS_FF_DEBUG 0x1L
+//#define VS_VERSION_INFO 0x1L
+//#define VS_FFI_FILEFLAGSMASK 0x17L
+//#define VER_PRIVATEBUILD 0x0L
+//#define VER_PRERELEASE 0x0L
+//#define VOS_NT_WINDOWS32 0x40004L
+//#define VFT_APP 0x1L
+
+// Local definitions
+#define VER_COMPANYNAME_STR "Dale Weiler, Wolfgang Bumiller"
+#define VER_FILEDESCRIPTION_STR "A standalone QuakeC VM binary executor"
+#define VER_PRODUCTNAME_STR "QCVM"
+#define VER_INTERNALNAME_STR "QCVM"
+#define VER_PRODUCTVERSION 0,3,6,0
+#define VER_PRODUCTVERSION_STR "0.3.6.0\0"
+#define VER_FILEVERSION 0,3,6,0
+#define VER_FILEVERSION_STR "0.3.6.0\0"
+#define VER_LEGALCOPYRIGHT_STR "\251 2019"
